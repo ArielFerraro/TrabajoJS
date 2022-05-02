a843a92a0e1a141339aafcde77105d449dce8476
Ariel Ferraro <ariel.ferraro74@hotmail.com>
2022-05-02 12:59:57 -0300

    se hicieron algunos cambios

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let op;
     document.querySelector('#calcular').addEventListener('click', () =>{
         const n1 = parseInt(document.querySelector('#n1').value);
         const n2 = parseInt(document.querySelector('#n2').value);
-        //const op = document.querySelector('#op').value;
+        
         let r;
         if(op == '+'){
             r = n1 + n2;
